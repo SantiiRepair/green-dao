75946fced38383f60dce7944c4657b866bb02c22
Santiago Ramirez <miguelsantiago1940@gmail.com>
2023-03-06 19:32:23 +0000

    change

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -19,10 +19,16 @@ const Tow = styled.div`
  display: flex;
  flex-direction: row;
  justify-content: center;
- position: fixed;
+ transform: translateY(100px);
+`
+const Tom = styled.div` 
+ display: flex;
+ flex-direction: row-reverse;
+ justify-content: center;
+ transform: translateY(100px);
 `
 
-function Home({ handleOpenModal }: Props) {
+function Mack({ handleOpenModal }: Props) {
   const { activateBrowserWallet, account } = useEthers();
 
   function handleConnectWallet() {
@@ -30,7 +36,7 @@ function Home({ handleOpenModal }: Props) {
   }
  return (
   <Flex
-  flexDirection='row'
+  flexDirection='column'
   display='flex'
   alignItems='center'
   bgColor='#282c34'
@@ -98,11 +104,102 @@ function Home({ handleOpenModal }: Props) {
     </Button>
   )}
     </Text>
+     <SimpleGlobe />
+    </Tow>
+  </Flex> 
+ ) 
+}
+
+function Meck({ handleOpenModal }: Props) {
+  const { activateBrowserWallet, account } = useEthers();
+
+  function handleConnectWallet() {
+    activateBrowserWallet();
+  }
+  return (
+  <Flex
+     flexDirection='column'
+     display='flex'
+     alignItems='center'
+     bgColor='#282c34'
+     bgImage='https://i.ibb.co/m4cyLkX/starry-sky-2675322-960-720.jpg'
+     bgSize='200vh'
+     bgRepeat='no-repeat'
+     minH='100vh'
+    >
+    <Tom>
+    <Text>
+     <Typography 
+     fontSize='30'
+     >Lets save our world, contribute now.</Typography>
+      <Typography 
+     fontSize='30'
+     >Time run and it dont stop.</Typography>
+       <Typography 
+     fontSize='30'
+     >If us dont do it, no body do it.</Typography>
+    { account ? (
+     <Button
+        bg="#2a4365"
+        mt='40'
+        border="1px solid transparent"
+        _hover={{
+          border: "1px",
+          borderStyle: "solid",
+          borderColor: "#2a4365",
+          backgroundColor: "#2a4365",
+        }}
+        borderRadius="0.75rem"
+        px={3}
+        color="#63b3ed"
+        fontSize="1.125rem"
+        fontWeight="500"
+        height='2.5rem'
+        minWidth='12.5rem'
+        href='/donate'
+      >
+      <Link href='/donate'>Donate Now</Link>
+      </Button>
+  ) : (
+      <Button
+      onClick={handleConnectWallet}
+      bg="#2a4365"
+      mt='40'
+      mb='3'
+      height='2.5rem'
+      minWidth='12.5rem'
+      color="#63b3ed"
+      fontSize="1.125rem"
+      fontWeight="500"
+      borderRadius="0.75rem"
+      border="1px solid transparent"
+      _hover={{
+        borderColor: "#2a4365",
+        color: "#4299e1",
+      }}
+      _active={{
+        backgroundColor: "blue.800",
+        borderColor: "blue.700",
+      }}
+    >
+      Connect Wallet
+    </Button>
+  )}
+    </Text>
+     <SimpleGlobe />
+    </Tom>
+   </Flex>
+  )
+}
+
+function Home() {
   
-    <SimpleGlobe />
-   </Tow>
+  return (  
+  <Flex bg='fixed' display='block'>
+    <Mack />
+    <Meck />
   </Flex>
- )
+  )
 }
 
 export default Home
